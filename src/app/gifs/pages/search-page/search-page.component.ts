8421e06b363abc3804ac235eaeefe3b5c6f6491c
Erick Hernandez <erickhernandez0492@gmail.com>
2025-03-06 11:34:16 +0100

    feat: creat search-page component

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { Gif } from '../../interfaces/gif.interface';
+import { GifService } from '../../service/gifs.service';
 
 @Component({
   selector: 'pages-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
 })
-export default class SearchPageComponent {}
+export default class SearchPageComponent {
+  gifsService = inject(GifService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+    this.gifsService.searchGifs(query).subscribe((resp) => {
+      this.gifs.set(resp);
+    });
+  }
+}
