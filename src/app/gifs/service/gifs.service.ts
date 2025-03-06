751b087ae8ee0de73d58a35e020dccedae5a81b4
Erick Hernandez <erickhernandez0492@gmail.com>
2025-03-06 11:38:38 +0100

    refactor:
    - Se creo el signal: searchHistory() y  searchHistoryKeys()
    - Se creo el observable para la busqueda de archivos hacia la API, searchGifs()
    - Creación del effect(), cuando el signal searchHistory(), cambie y asi poder guardar los datos en el localStorage
    - Creación de la función loadFromLocalStorage() para obtener los datos del localStorage y parsearlos a un objeto
    - Creación de la función getHistoryGifs(), para devolver el array del localStorage de acuerdo al searchHistory seleccionado

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -1,9 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+
 import { environment } from '@environments/environment';
+import { map, Observable, tap } from 'rxjs';
 import { Gif } from '../interfaces/gif.interface';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
+
+const GIF_KEY = 'gifs';
+
+const loadFromLocalStorage = () => {
+  const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}';
+  const gifs = JSON.parse(gifsFromLocalStorage);
+
+  console.log(gifs);
+  return gifs;
+};
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -12,9 +24,18 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal<boolean>(true);
 
+  searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+
   constructor() {
     this.loadTrendingGifs();
   }
+
+  saveGifsToLocalStorage = effect(() => {
+    const historyString = JSON.stringify(this.searchHistory());
+
+    localStorage.setItem(GIF_KEY, historyString);
+  });
 
   loadTrendingGifs() {
     this.http
@@ -26,5 +47,34 @@ export class GifService {
         this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
       });
+  }
+
+  searchGifs(query: string): Observable<Gif[]> {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: { api_key: environment.giphyApiKey, q: query, limit: 20 },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+
+        // TODO: Historial
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
+        })
+      );
+
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   this.trendingGifs.set(gifs);
+    //   this.trendingGifsLoading.set(false);
+    // });
+  }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 }
