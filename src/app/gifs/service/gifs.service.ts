d512c39ea448bda6e17e838e9d8b46f2e5daa12a
Erick Hernandez <erickhernandez0492@gmail.com>
2025-03-07 11:01:36 +0100

    feat: add trendingGifGroup and Masonry grid
    - Create signal trendingGifGroup(), for Masonry grid

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -13,7 +13,6 @@ const loadFromLocalStorage = () => {
   const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}';
   const gifs = JSON.parse(gifsFromLocalStorage);
 
-  console.log(gifs);
   return gifs;
 };
 
@@ -22,7 +21,18 @@ export class GifService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading = signal<boolean>(true);
+  trendingGifsLoading = signal<boolean>(false);
+  private trendingPage = signal(0);
+
+  //[[gif, gif, gif], [gif, gif, gif], [gif, gif, gif]]
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+    return groups; //[g1,g2,g3],[g4,g5,g6]
+  });
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
@@ -38,13 +48,22 @@ export class GifService {
   });
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
-        params: { api_key: environment.giphyApiKey, limit: 20 },
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          offset: this.trendingPage() * 20,
+        },
       })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-        this.trendingGifs.set(gifs);
+        this.trendingGifs.update((currentGifs) => [...currentGifs, ...gifs]);
+        this.trendingPage.update((current) => current + 1);
         this.trendingGifsLoading.set(false);
       });
   }
